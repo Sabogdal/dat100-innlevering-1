8c2a296361df0c3596f9e87a97589718bf269751
Sabogdal <thomasgs.91@gmail.com>
2025-09-02 15:07:13 +0200

    OppgO1.java

--- OppgO1.java
+++ OppgO1.java
@@ -13,19 +13,19 @@ public class OppgO1 {
                     System.out.println("Du betaler 0 kr i skatt");
                 }
                     else if(tall >= 217401 && tall <= 306050) {
-                        System.out.println("Din trinnskatt er 1,7%, og du betaler" + tall*0.017 + "kr. i trinnskatt");
+                        System.out.println("Din trinnskatt er 1,7%, og du betaler nøyaktig " + tall*0.017 + " kr. i trinnskatt");
                         }
                     else if (tall >= 306051 && tall <= 697150) {
-                            System.out.println("Din trinnskatt er 4,0%, og du betaler" + tall*0.04 +"kr. i trinnskatt");
+                            System.out.println("Din trinnskatt er 4,0%, og du betaler nøyaktig " + tall*0.04 +"kr. i trinnskatt");
                         }
                     else if (tall >= 697151 && tall <= 942400) {
-                            System.out.println("Din trinnskatt er 13,7%, og du betaler" + tall*0.137 + "kr. i trinnskatt");
+                            System.out.println("Din trinnskatt er 13,7%, og du betaler nøyaktig " + tall*0.137 + " kr. i trinnskatt");
                         }
                     else if (tall >= 942401 && tall <= 1410750) {
-                            System.out.println("Din trinnskatt er 16,7%, og du betaler" + tall*0.167 + " kr. i trinnskatt");
+                            System.out.println("Din trinnskatt er 16,7%, og du betaler nøyaktig " + tall*0.167 + " kr. i trinnskatt");
                         }
                     else if (tall >= 1410751 && tall <= 10000000) {
-                            System.out.println("Din trinnskatt er 17,7%, og du betaler" + tall*0.177 + "kr. i trinnskatt");
+                            System.out.println("Din trinnskatt er 17,7%, og du betaler nøyaktig " + tall*0.177 + " kr. i trinnskatt");
                         }
                     else {
                         System.out.println("Du må skrive tall");
@@ -34,4 +34,3 @@ public class OppgO1 {
 
                 }
         }
-
